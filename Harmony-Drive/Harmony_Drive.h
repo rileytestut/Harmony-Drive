d6fd8572d4b8c27b2699e86f6668234089670563
Riley Testut <riley@rileytestut.com>
2018-01-22 14:09:22 -0800

    Initial Commit

---
+++ Harmony-Drive/Harmony_Drive.h
@@ -0,0 +1,19 @@
+//
+//  Harmony_Drive.h
+//  Harmony-Drive
+//
+//  Created by Riley Testut on 1/22/18.
+//  Copyright © 2018 Riley Testut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Harmony_Drive.
+FOUNDATION_EXPORT double Harmony_DriveVersionNumber;
+
+//! Project version string for Harmony_Drive.
+FOUNDATION_EXPORT const unsigned char Harmony_DriveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Harmony_Drive/PublicHeader.h>
+
+
