435084163728c66d99351cb44bb41fc13584ce3c
Riley Testut <riley@rileytestut.com>
2018-11-27 14:17:30 -0800

    Adds ability to fetch versions of remote records

---
+++ Harmony-Drive/DriveService+Versions.swift
@@ -0,0 +1,57 @@
+//
+//  DriveService+Versions.swift
+//  Harmony-Drive
+//
+//  Created by Riley Testut on 11/20/18.
+//  Copyright © 2018 Riley Testut. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+import Harmony
+import Roxas
+
+import GoogleDrive
+
+public extension DriveService
+{
+    public func fetchVersions(for record: RemoteRecord, completionHandler: @escaping (Result<[Version]>) -> Void) -> Progress
+    {
+        let progress = Progress.discreteProgress(totalUnitCount: 1)
+        
+        guard let managedRecord = record.managedRecord else {
+            completionHandler(.failure(AnyError(code: .nilManagedRecord)))
+            return progress
+        }
+                
+        let query = GTLRDriveQuery_RevisionsList.query(withFileId: record.identifier)
+        
+        let ticket = self.service.executeQuery(query) { (ticket, object, error) in
+            guard error == nil else {
+                if let error = error as NSError?, error.domain == kGTLRErrorObjectDomain && error.code == 404
+                {
+                    return completionHandler(.failure(FetchVersionsError(record: managedRecord, code: .fileDoesNotExist)))
+                }
+                else
+                {
+                    return completionHandler(.failure(FetchVersionsError(record: managedRecord, code: .any(error!))))
+                }
+            }
+            
+            guard let revisionList = object as? GTLRDrive_RevisionList, let revisions = revisionList.revisions else {
+                return completionHandler(.failure(FetchVersionsError(record: managedRecord, code: .invalidResponse)))
+            }
+            
+            let versions = revisions.lazy.compactMap(Version.init(revision:)).reversed()
+            completionHandler(.success(Array(versions)))
+        }
+        
+        progress.cancellationHandler = {
+            ticket.cancel()
+            completionHandler(.failure(FetchVersionsError(record: managedRecord, code: .cancelled)))
+        }
+        
+        return progress
+    }
+}
---
+++ Harmony-Drive/Extensions/Version+Revision.swift
@@ -0,0 +1,27 @@
+//
+//  Version+Revision.swift
+//  Harmony-Drive
+//
+//  Created by Riley Testut on 11/20/18.
+//  Copyright © 2018 Riley Testut. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+import Harmony
+
+import GoogleDrive
+
+extension Version
+{
+    init?(revision: GTLRDrive_Revision)
+    {
+        guard
+            let identifier = revision.identifier,
+            let date = revision.modifiedTime?.date
+        else { return nil }
+        
+        self.init(identifier: identifier, date: date)
+    }
+}
