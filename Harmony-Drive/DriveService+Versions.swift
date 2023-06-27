1258b528de0ae3806d6a3d6f8b071119c59476fe
Riley Testut <riley@rileytestut.com>
2023-06-27 18:41:46 -0500

    Implements Service.fetchVersions(forRemoteFile:completionHandler:)

--- Harmony-Drive/DriveService+Versions.swift
+++ Harmony-Drive/DriveService+Versions.swift
@@ -34,6 +34,8 @@ public extension DriveService
                         
                         let versions = revisions.lazy.compactMap(Version.init(revision:)).reversed()
                         completionHandler(.success(Array(versions)))
+                        
+                        progress.completedUnitCount = 1
                     }
                     catch
                     {
@@ -50,6 +52,44 @@ public extension DriveService
         catch
         {
             completionHandler(.failure(RecordError(record, error)))
+        }
+        
+        return progress
+    }
+    
+    func fetchVersions(for remoteFile: RemoteFile, completionHandler: @escaping (Result<[Version], FileError>) -> Void) -> Progress
+    {
+        let progress = Progress.discreteProgress(totalUnitCount: 1)
+        let fileIdentifier = remoteFile.identifier
+
+        do
+        {
+            let query = GTLRDriveQuery_RevisionsList.query(withFileId: remoteFile.remoteIdentifier)
+            
+            let ticket = self.service.executeQuery(query) { (ticket, object, error) in
+                do
+                {
+                    let revisions = try self.process(Result((object as? GTLRDrive_RevisionList)?.revisions, error))
+                    
+                    let versions = revisions.lazy.compactMap(Version.init(revision:)).reversed()
+                    completionHandler(.success(Array(versions)))
+                    
+                    progress.completedUnitCount = 1
+                }
+                catch
+                {
+                    completionHandler(.failure(FileError(fileIdentifier, error)))
+                }
+            }
+            
+            progress.cancellationHandler = {
+                ticket.cancel()
+                completionHandler(.failure(FileError(fileIdentifier, GeneralError.cancelled)))
+            }
+        }
+        catch
+        {
+            completionHandler(.failure(FileError(fileIdentifier, error)))
         }
         
         return progress
